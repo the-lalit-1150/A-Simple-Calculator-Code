08e751674a35e11ab3b3ff7b1e61a275d034dd9e
Lalit Kewat ( SAHIL ) <ssir88467@gmail.com>
2026-03-02 12:55:19 +0530

    Add files via upload

---
+++ C++ Calculator.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num1 , num2 , re ;
+    char op ;
+    cout<<"Enter First Number: ";
+    if(!(cin>>num1)) {
+        cout<<"----- Enter Valid Number -----";
+        return 1;
+    }
+    cout<<"Enter Second Number: ";
+    if(!(cin>>num2)) {
+        cout<<"----- Enter Valid Number -----";
+        return 1;
+    }
+    cout<<"Enter Operator '+ , - , * , /': ";
+    if(!(cin>>op)) {
+        cout<<"----- Enter Valid Operator -----";
+        return 1;
+    }
+    switch (op)
+    {
+    case '+':
+    re = num1 + num2;
+    cout<<re<<endl;
+    break;
+    
+    case '-':
+    re = num1 - num2;
+    cout<<re<<endl;
+    break;
+    
+    case '*':
+    re = num1 * num2;
+    cout<<re<<endl;
+    break;
+    
+    case '/':
+    if(num2 != 0){
+    re = (float) num1 / num2;
+    cout<<re<<endl;
+    }
+    else{
+        cout<<"The Second Should Not Be Zero!"<<endl;
+    }
+    break;
+    
+    default:
+    cout<<"----- Invalid Operator -----"<<endl;
+    
+    }
+    
+    return 0;
+}
